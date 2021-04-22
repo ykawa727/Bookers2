2d7c9b6c4a73e7c6e3f95f2423ecb0cb631f9865
ykawa727 <y.aikawa727@gmail.com>
2021-04-22 13:09:13 +0000

    [Learning3] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'homes#top'
+
+  get 'homes/about', to: 'homes#about', as: 'about_home'
+  # get 'homes/about' => 'homes#about', as: 'about_home'
+
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+
+  resources :users, only: [:show, :index]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_22_012104) do
+ActiveRecord::Schema.define(version: 2021_04_22_054021) do
+
+  create_table "books", force: :cascade do |t|
+    t.text "title"
+    t.string "bookpost_id"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
