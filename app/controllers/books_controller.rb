e557a2a1eac67324d4acb7b095361a8717fbcb58
ykawa727 <y.aikawa727@gmail.com>
2021-04-25 13:15:34 +0000

    [Learning5] 4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,15 +3,26 @@ class BooksController < ApplicationController
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(current_user)
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
+    @user = User.find(current_user.id)
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
   def index
     @user = User.all
-  end 
+  end
+
+  private
+  def user_params
+    params.require(:list).permit(:title, :body, :profile_image)
+  end
+
 end
